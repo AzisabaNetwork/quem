d9f538c483cc0fcea9ba617e33f9a04d109481b6
tksimeji <tksimeji@outlook.com>
2025-02-12 19:21:17 +0900

    clean: removed test code for quest category

--- src/main/kotlin/net/azisaba/quem/registry/QuestCategories.kt
+++ src/main/kotlin/net/azisaba/quem/registry/QuestCategories.kt
@@ -10,9 +10,4 @@ object QuestCategories: Registry<QuestCategory>() {
     val GENERAL = register(QuestCategory(Key.key("${Quem.PLUGIN_ID}:general"), Component.text("General"), Material.CHEST))
     val DAILY = register(QuestCategory(Key.key("${Quem.PLUGIN_ID}:daily"), Component.text("Daily"), Material.CLOCK))
     val STORY = register(QuestCategory(Key.key("${Quem.PLUGIN_ID}:story"), Component.text("Story"), Material.ENCHANTED_BOOK))
-    val EXAMPLE1 = register(QuestCategory(Key.key("${Quem.PLUGIN_ID}:example"), Component.text("Example"), Material.PAPER))
-    val EXAMPLE2 = register(QuestCategory(Key.key("${Quem.PLUGIN_ID}:example2"), Component.text("Example"), Material.PAPER))
-    val EXAMPLE3 = register(QuestCategory(Key.key("${Quem.PLUGIN_ID}:example3"), Component.text("Example"), Material.PAPER))
-    val EXAMPLE4 = register(QuestCategory(Key.key("${Quem.PLUGIN_ID}:example4"), Component.text("Example"), Material.PAPER))
-    val EXAMPLE6 = register(QuestCategory(Key.key("${Quem.PLUGIN_ID}:example6"), Component.text("Example"), Material.PAPER))
 }
